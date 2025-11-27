506cf58b58ec55fa5b4e41388a8cc3a7f62b7f7f
jignesh <jignesh.oceantech@gmail.com>
2025-11-27 15:03:26 +0530

    change equipment e

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -9,9 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Equipment extends Model
 {
-    
     use HasFactory;
-
     protected $table = Tables::EQUIPMENTS;
     protected $guarded = [];
 }
